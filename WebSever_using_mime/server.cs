521e812b0adca7537ab9256c78ebd3df06e42c0b
vjola <vjola@tavisca.com>
2019-08-02 11:03:21 +0530

    Add project files.

---
+++ WebSever_using_mime/server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSever_using_mime
+{
+    public class Server
+    {
+        TcpListener tcp 
+        public int port = 1490;
+        public Server()
+        {
+            tcp = new TcpListener(1562);
+            tcp.Start();
+            Console.WriteLine("Server running");
+
+        }
+
+    }
+}
